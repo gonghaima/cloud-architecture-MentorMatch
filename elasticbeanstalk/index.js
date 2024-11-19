0e8a4e6f87eb7a5ed6a6b65eb25732e729413b44
Steven <gonghaima@hotmail.com>
2024-11-20 01:46:12 +1100

    node app - pointing to react app static file

--- elasticbeanstalk/index.js
+++ elasticbeanstalk/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'MentorMatchApp', 'dist')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'MentorMatchApp', 'dist', 'index.html'));
 });
 
 app.listen(port, () => {
